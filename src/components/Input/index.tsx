09de6f2e87df94c47e2d8a3b4a1f2bbc4f644ddc
igulevich10 <78545961+igulevich10@users.noreply.github.com>
2023-06-12 18:38:46 +0300

    create UI components

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,27 @@
+import React, { InputHTMLAttributes } from "react";
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+	label: string;
+}
+
+const Input = ({ type = "text", placeholder, value, name, onChange, label }: InputProps) => {
+	return (
+		<div className="field">
+			<div className="control">
+				<label htmlFor={name}>{label}</label>
+				<input 
+					className="input"
+					type={type}
+					placeholder={placeholder}
+					value={value}
+					name={name}
+					id={name}
+					onChange={onChange}
+					required
+				/>
+			</div>
+		</div>
+	)
+};
+
+export default Input;
